0ce4b9f9cf527f80e31d08b1a8590fc4aaaf1588
ilya <ilyasikirytski@gmail.com>
2022-07-21 17:47:17 +0300

    Change comment

--- app/src/main/java/com/example/dagger_2_example_2/Car.kt
+++ app/src/main/java/com/example/dagger_2_example_2/Car.kt
@@ -3,7 +3,7 @@ package com.example.dagger_2_example_2
 import javax.inject.Inject
 import javax.inject.Provider
 import dagger.Lazy
-
+// Provider return new instance of the object each time it's called
 class Car @Inject constructor(private var engine: Provider<Engine>) {
     var key: Key? = null
         @Inject set
--- app/src/main/java/com/example/dagger_2_example_2/Engine.kt
+++ app/src/main/java/com/example/dagger_2_example_2/Engine.kt
@@ -4,7 +4,7 @@ import android.util.Log
 import javax.inject.Inject
 import javax.inject.Provider
 import dagger.Lazy
-
+// Lazy return previously cached instance of the object each time it's called
 class Engine @Inject constructor(private var fuel: Lazy<Fuel>) {
     fun start() {
 //
